5e6cc7ee62c44e8db4ae244649ee42107974da2b
zachyboy-92 <zakaria_0456@hotmail.com>
2021-09-09 12:28:30 -0400

    modified repo

--- src/components/GetQuestion.js
+++ src/components/GetQuestion.js
@@ -1,72 +1,111 @@
 import React, { Component } from "react";
+import "./GetQuestion.css";
+
+let answer;
 
 class GetQuestion extends Component {
   constructor() {
     super();
     this.state = {
       data: [],
-      enteredAnswer: "",
       correctAnswer: "",
       points: 0,
       getQuestion: false,
-      getAnswer: false,
     };
   }
 
   async buttonHandler() {
     const url = await fetch("http://jservice.io/api/random");
     const response = await url.json();
-    console.log(response);
+
     this.setState({ data: response });
     this.setState({ getQuestion: true });
   }
 
-  answerHander(e) {
-    this.setState({ enteredAnswer: e.target.value });
+  submitHandler() {
+    this.state.data.map((dt) => {
+      this.setState({ correctAnswer: dt.answer });
+      answer = <p className="answer-holder">{this.state.correctAnswer}</p>;
+      return answer;
+    });
   }
 
-  //   checkAnswer() {
-  //     this.state.data.map((dt) => this.setState({ answer: dt.answer }));
-  //     if (this.state.correctAnswer === this.state.enteredAnswer) {
-  //       this.setState({ points: this.state.points + 1 });
-  //     }
-  //   }
+  increaseScoreHandler() {
+    this.setState({ points: this.state.points + 1 });
+  }
 
-  submitHandler() {
-    this.state.data.map((dt) => this.setState({ answer: dt.answer }));
-    if (this.state.correctAnswer === this.state.enteredAnswer) {
-      this.setState({ points: this.state.points + 1 });
-    } else {
-      this.setState({ points: this.state.points - 1 });
-    }
+  decreaseScoreHandler() {
+    this.setState({ points: this.state.points - 1 });
+  }
+
+  resetScoreHandler() {
+    this.setState({ points: 0 });
   }
 
   render() {
     return (
       <div>
-        <h2>Points: {this.state.points}</h2>
-        <button onClick={this.buttonHandler.bind(this)}>Get Question</button>
+        <div className="score-container">
+          <span className="score-holder">Score: </span>
+          <span className="score">{this.state.points}</span>
+        </div>
+        <div>
+          <button
+            className="btn increase"
+            onClick={this.increaseScoreHandler.bind(this)}
+          >
+            Increase
+          </button>
+          <button
+            className="btn decrease"
+            onClick={this.decreaseScoreHandler.bind(this)}
+          >
+            Decrease
+          </button>
+          <button
+            className="btn reset"
+            onClick={this.resetScoreHandler.bind(this)}
+          >
+            Reset
+          </button>
+        </div>
+        <h3 className="play-header">Let's Play</h3>
+        <button
+          className="btn question-btn"
+          onClick={this.buttonHandler.bind(this)}
+        >
+          Get Question
+        </button>
         {this.state.getQuestion
           ? this.state.data.map((dt) => {
               return (
-                <div>
-                  <p>Category: {dt.category.title}</p>
-                  <p>Points: {dt.value}</p>
-                  <p>Answer: {dt.question}</p>
+                <div className="question-container" key={dt.id}>
+                  <div className="question-holder">
+                    <span className="holder">Category: </span>
+                    <span className="question">{dt.category.title}</span>
+                  </div>
+
+                  <div className="question-holder">
+                    <span className="holder">Points: </span>
+                    <span className="question">{dt.value}</span>
+                  </div>
+
+                  <div className="question-holder">
+                    <span className="holder">Answer: </span>
+                    <span className="question">{dt.question}</span>
+                  </div>
                 </div>
               );
             })
           : " "}
 
-        {this.state.getQuestion ? (
-          <form onSubmit={this.submitHandler.bind(this)}>
-            <label htmlFor="answer">Insert Answer: </label>
-            <input id="answer" onChange={this.answerHander.bind(this)}></input>
-            <button>Submit</button>
-          </form>
-        ) : (
-          ""
-        )}
+        <button
+          className="btn answer-btn"
+          onClick={this.submitHandler.bind(this)}
+        >
+          Submit
+        </button>
+        {answer}
       </div>
     );
   }
